e239b9cda93c2a07e6c6d1fe26b131eee7a116f0
Alex Tsang <alextsang@live.com>
2019-10-22 09:25:13 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# Namecheap-DDNS #
+
+## Description ##
+
+A small script for updating DNS records on Namecheap.
+
+Tested on OpenBSD and Alpine Linux.
+
+## Requirements ##
+
+* (None.)
+
+## Installation ##
+
+0. Download the script.
+1. Add configuration files in the `configs` directory.
+
+## Usage ##
+
+Each configuration file in the `configs` directory should be a regular,
+readable, non-hidden file, and contains the following properties:
+
+* `domain`: Domain name.
+* `password`: Password for updating the DNS record using the Dynamic DNS feature
+provided by Namecheap.
+* `host`: Host. Use `@` for the domain itself.
+* `log`: Log filename. The log file is put under the `logs` directory.
+
+A file `.example.com.config` is put under the `configs` as an example.
+
+A cron job can be setup to run the script periodically. For example, add a cron
+job by `crontab -e`:
+
+    # Run the script located at /path/to/update.sh every 1 hour.
+    0 * * * * /bin/sh /path/to/update.sh
+
+## License ##
+
+[The 3-Clause BSD License](http://opensource.org/licenses/BSD-3-Clause)
---
+++ update.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+#
+# Script for updating DNS records on Namecheap.
+# Author: Alex Tsang <alextsang@live.com>
+# License: The 3-Clause BSD License
+
+# Strict mode.
+set -e
+set -u
+
+scriptDirectory="$(cd "$(dirname "$0")"; pwd)"
+domainsDirectory="${scriptDirectory}/configs"
+logsDirectory="${scriptDirectory}/logs"
+
+apiURL="https://dynamicdns.park-your-domain.com/update"
+
+configurations=$(ls "${domainsDirectory}")
+for configuration in ${configurations}; do
+  filePath="${domainsDirectory}/${configuration}"
+  # Ignore anything other than regular files, such as directories.
+  if [ ! -f "${filePath}" ]; then
+    continue
+  fi
+
+  domain=
+  password=
+  host=
+  log=
+
+  . "${filePath}"
+
+  url="${apiURL}?domain=${domain}&password=${password}&host=${host}"
+
+  (
+    # Log update time.
+    date -u '+%Y-%m-%dT%H:%M:%SZ'
+
+    if [ -n "$(command -v ftp)" ]; then
+      ftp -M -o - -U '' "${url}"
+    elif [ -n "$(command -v wget)" ]; then
+      wget -q -O - --user-agent='' "${url}"
+    else
+      echo 'Neither ftp nor wget is found.'.
+    fi
+
+    # New line.
+    echo ''
+  ) >> "${logsDirectory}/${log}"
+done
